5d8ce6e103aadefd58003a8b1a139fa89262baed
Mara <mara-li@outlook.fr>
2023-05-14 22:56:33 +0200

    ci: fix dev & ci

--- dev.js
+++ dev.js
@@ -9,20 +9,45 @@ let vaultDev = process.env.VAULT_DEV || "";
 //get args "--prod" or "--dev"
 const args = process.argv.slice(2);
 
+c.theme({
+	danger: c.red,
+	dark: c.dim.gray,
+	disabled: c.gray,
+	em: c.italic,
+	heading: c.bold.underline,
+	info: c.cyan,
+	muted: c.dim,
+	primary: c.blue,
+	strong: c.bold,
+	success: c.green.bold,
+	underline: c.underline,
+	warning: c.yellow.underline
+});
+
 if (args.length > 0 && args[0] === "--prod") {
 	vaultDev = process.env.VAULT || "";
 }
-let msg = vaultDev.trim().length > 0 ? `-v ${c.underline(vaultDev)}` : "";
-const cmd = vaultDev.trim().length > 0 ? `-v ${vaultDev}` : "";
 
-const pluginDir = path.join(vaultDev, ".obsidian", "plugins", "copy-reading-in-markdown", ".hotreload");
-
-if (!fs.existsSync(pluginDir)) {
-	console.log(c.yellow.underline(".hotreload"));
-	fs.mkdirSync(pluginDir);
+if (vaultDev.trim().length > 0) {
+	const filePath = path.join(vaultDev, ".obsidian", "plugins", "copy-reading-in-markdown", ".hotreload");
+	if (!fs.existsSync(filePath)) {
+		console.log(`${c.danger.bold("❌")} ${c.danger(".hotreload file not found. Creating it...")}`);
+		fs.writeFile(filePath, "", (err) => {
+			if (err) {
+				console.error(err);
+			}
+		});
+		setTimeout(function () {
+		}, 1000);
+		console.log(`✔️ ${c.success(".hotreload file created.")}`);
+		console.log();
+	}
 }
 
+let msg = vaultDev.trim().length > 0 ? `-v ${c.underline.bold.blue(vaultDev)}` : "";
+const cmd = vaultDev.trim().length > 0 ? `-v ${vaultDev}` : "";
+
 const command = `obsidian-plugin dev --with-stylesheet src/styles.css src/main.ts ${cmd}`;
-console.log(c.blue.italic(`${c.bold.red(">")} obsidian-plugin dev ${c.yellowBright.underline("--with-stylesheet src/styles.css")} src/main.ts ${msg}`));
+console.log(c.info.italic(`${c.bold(">")} obsidian-plugin dev ${c.dark.underline("--with-stylesheet src/styles.css")} src/main.ts ${msg}`));
 execSync(command, { stdio: "inherit" });
 
