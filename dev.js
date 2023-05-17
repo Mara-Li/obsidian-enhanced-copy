b948a973d6277d456e5ca2cb2468a86d31570d03
Mara <mara-li@outlook.fr>
2023-05-18 00:38:37 +0200

    docs(translation): fix typo in fr

--- dev.js
+++ dev.js
@@ -37,8 +37,8 @@ if (vaultDev.trim().length > 0) {
 				console.error(err);
 			}
 		});
-		setTimeout(function () {
-		}, 1000);
+		// eslint-disable-next-line @typescript-eslint/no-empty-function
+		setTimeout(function () {}, 1000);
 		console.log(`✔️ ${c.success(".hotreload file created.")}`);
 		console.log();
 	}
