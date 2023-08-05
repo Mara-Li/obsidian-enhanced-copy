0e32e9e514907a74a225fadd6ea5ea96df2518e0
Mara <mara-li@outlook.fr>
2023-08-05 23:41:54 +0200

    fix(logs): fix process not exist on mobile
    check Platform before process.env

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -2,16 +2,18 @@
  * @author Trganda
  * @source https://github.com/trganda/obsidian-attachment-management/blob/main/src/log.ts
  */
+import { Platform } from "obsidian";
 
-export const DEBUG = process.env.BUILD_ENV;
 
 export function devLog(...args: unknown[]) {
-	if (DEBUG) {
-		let callFunction = new Error().stack?.split("\n")[2].trim();
-		callFunction = callFunction?.substring(callFunction.indexOf("at ") + 3, callFunction.lastIndexOf(" ("));
-		callFunction = callFunction.replace("Object.callback", "");
-		callFunction = callFunction.length > 0 ? callFunction : "main";
-		const date = new Date().toISOString().slice(11, 23);
-		console.log(`[${date}](${callFunction}):\n`, ...args);
+	if (Platform.isDesktop) {
+		if (process.env.BUILD_ENV) {
+			let callFunction = new Error().stack?.split("\n")[2].trim();
+			callFunction = callFunction?.substring(callFunction.indexOf("at ") + 3, callFunction.lastIndexOf(" ("));
+			callFunction = callFunction.replace("Object.callback", "");
+			callFunction = callFunction.length > 0 ? callFunction : "main";
+			const date = new Date().toISOString().slice(11, 23);
+			console.log(`[${date}](${callFunction}):\n`, ...args);
+		}
 	}
 }
